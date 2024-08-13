32862d173c7cb1031629efadaaf89a018db3dd62
Yuri <wall.mart.boss@proton.me>
2024-08-13 04:12:42 +0300

    basic

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+let mixedType: string | number;
+mixedType = "test";
+mixedType = 3;
+
+
+type choice = 'enable' | 'disable';
+let value: choice;
+value = 'enable';
+value = 'disable';
---
+++ src/basic/4.ts
@@ -0,0 +1,16 @@
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+showMessage("Hello, world!");
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+};
+console.log(calc(3, 6));
+
+
+function customError(): never {
+  throw new Error('Error');
+};
+console.log(customError());
